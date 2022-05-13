7672c7745b4178b5c65b5b6b5c397fa539ee895f
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-05-13 14:40:03 +0000

    menu generator

--- Console/In/InGeneratorMenu.php
+++ Console/In/InGeneratorMenu.php
@@ -31,6 +31,9 @@ class InGeneratorMenu extends Command
         $menuTitleTemplate = file_get_contents(
             __DIR__ . '/templates/MenuTitle.txt'
         );
+        $menuFolderTemplate = file_get_contents(
+            __DIR__ . '/templates/MenuFolder.txt'
+        );
 
         $menuFile = $_ENV['NAE_SFS_CP_STORAGE_PATH'] . '/menu.json';
         $menuItems = json_decode(
@@ -107,6 +110,73 @@ class InGeneratorMenu extends Command
                 [
                     $compName,
                     $menuItem['config']['title']
+                ],
+                $menuTitleTemplate
+            );
+
+            if ($localContents !== $generatedContent) {
+                file_put_contents(
+                    $fileName,
+                    $generatedContent
+                );
+            }
+        }
+
+        // MENU FOLDER
+        $menuFolderFile = $_ENV['NAE_SFS_CP_STORAGE_PATH'] . '/menu-folder.json';
+        $menuFolderItems = json_decode(
+            file_get_contents($menuFolderFile),
+            true
+        );
+
+        $generatedPath = LocalConfigUtils::getFrontendGeneratedPath() . '/menu/folders';
+        if (!$fs->exists($generatedPath)) {
+            $fs->mkdir($generatedPath);
+        }
+
+        foreach ($menuFolderItems as $menuItem) {
+            $compName = $this->menuService->componentNameForMenuFolder($menuItem);
+
+            $fileName = $generatedPath . '/' . $compName . '.tsx';
+
+            $tpImports = '';
+            $tpChilds = '';
+
+            foreach ($menuItem['config']['items'] as $itemId) {
+                if (mb_strpos($itemId, 'separartor') === 0) {
+                    $tpChilds .= "<MenuSpacer />" . PHP_EOL;
+                } else {
+                    foreach ($menuItems as $m) {
+                        if ($m['id'] === $itemId) {
+                            $menuCompName = $this->menuService->componentNameForMenu($m);
+
+                            $tpChilds .= "<" . $menuCompName . " />" . PHP_EOL;
+
+                            $tpImports .= "import " .$menuCompName." from \"../items/" . $menuCompName . "\" " . PHP_EOL;
+                        }
+                    }
+                }
+            }
+
+            $localContents = '';
+            if ($fs->exists($fileName)) {
+                $localContents = file_get_contents($fileName);
+            }
+
+            $generatedContent = str_replace(
+                [
+                    'TP_COMP_NAME',
+                    'TP_TITLE',
+                    'TP_ICON',
+                    'TP_IMPORTS',
+                    'TP_CHILDS'
+                ],
+                [
+                    $compName,
+                    $menuItem['config']['title'],
+                    $menuItem['config']['icon'],
+                    $tpImports,
+                    $tpChilds
                 ],
                 $menuTitleTemplate
             );
