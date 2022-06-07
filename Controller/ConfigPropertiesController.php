e7a8e80082f882870f61eef25512928c91350ee9
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-06-07 14:58:39 +0000

    files widget generator

--- Controller/ConfigPropertiesController.php
+++ Controller/ConfigPropertiesController.php
@@ -13,6 +13,34 @@ use Symfony\Component\HttpFoundation\Request;
 class ConfigPropertiesController extends ConfigBaseController
 {
     /**
+     * @Route(path="/for-filter", methods={"POST"})
+     */
+    public function getPropertiesForFilter(Request $request, PropertiesUtils $propertiesUtils)
+    {
+        $request = $this->transformJsonBody($request);
+
+        $schema = $request->get('schema');
+
+        $schemaProperties = $this->schemaPropetiesForFilter($schema, $propertiesUtils);
+
+        $rels = $this->relPropertiesForFilter($schema, $propertiesUtils);
+        foreach ($rels as $relProperty) {
+            $relSchemaProperties = $this->schemaPropetiesForSort($relProperty['format'], $propertiesUtils);
+            foreach ($relSchemaProperties as $relSchemaProperty) {
+                $key = explode(".", $relSchemaProperty['value']);
+                $title = $relProperty['title'] . ' -> ' . $relSchemaProperty['label'];
+                $schemaProperties[] = [
+                    'value' => 'i.' . $relProperty['key'] . '.' . $key[1],
+                    'label' => $title,
+                ];
+            }
+        }
+
+
+        return $this->json(['data' => array_values($schemaProperties)]);
+    }
+
+    /**
      * @Route(path="/for-sort", methods={"POST"})
      */
     public function getPropertiesForSort(Request $request, PropertiesUtils $propertiesUtils)
@@ -23,7 +51,7 @@ class ConfigPropertiesController extends ConfigBaseController
 
         $schemaProperties = $this->schemaPropetiesForSort($schema, $propertiesUtils);
 
-        $rels = $this->relPropetiesForSort($schema, $propertiesUtils);
+        $rels = $this->relPropertiesForSort($schema, $propertiesUtils);
         foreach ($rels as $relProperty) {
             $relSchemaProperties = $this->schemaPropetiesForSort($relProperty['format'], $propertiesUtils);
             foreach ($relSchemaProperties as $relSchemaProperty) {
@@ -83,7 +111,36 @@ class ConfigPropertiesController extends ConfigBaseController
         return $schemaProperties;
     }
 
-    protected function relPropetiesForSort(string $schema, PropertiesUtils $propertiesUtils)
+    protected function schemaPropetiesForFilter(string $schema, PropertiesUtils $propertiesUtils)
+    {
+        $schemaProperties = array_filter(
+            $propertiesUtils->getProperties(),
+            function ($property) use ($schema) {
+                return ($property['schema'] === $schema &&
+                    $property['key'] !== 'id' &&
+                    isset($property['isDb']) &&
+                    $property['isDb'] &&
+                    isset($property['available']) &&
+                    $property['available']['filter'] &&
+                    $property['type'] !== 'rel' &&
+                    $property['type'] !== 'array'
+                );
+            }
+        );
+
+        $schemaProperties = array_map(
+            function ($property) {
+                return [
+                    'value' => 'i.' . $property['key'],
+                    'label' => $property['title'],
+                ];
+            },
+            $schemaProperties
+        );
+        return $schemaProperties;
+    }
+
+    protected function relPropertiesForSort(string $schema, PropertiesUtils $propertiesUtils)
     {
         $schemaProperties = array_filter(
             $propertiesUtils->getProperties(),
@@ -91,6 +148,22 @@ class ConfigPropertiesController extends ConfigBaseController
                 return ($property['schema'] === $schema &&
                     isset($property['available']) &&
                     $property['available']['sort'] &&
+                    $property['type'] == 'rel'
+                );
+            }
+        );
+
+        return $schemaProperties;
+    }
+
+    protected function relPropertiesForFilter(string $schema, PropertiesUtils $propertiesUtils)
+    {
+        $schemaProperties = array_filter(
+            $propertiesUtils->getProperties(),
+            function ($property) use ($schema) {
+                return ($property['schema'] === $schema &&
+                    isset($property['available']) &&
+                    $property['available']['filter'] &&
                     $property['type'] == 'rel'
                 );
             }
