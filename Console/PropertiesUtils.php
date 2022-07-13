9ca959bd361b6ae9cbed2a373804286a7092b590
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-07-13 12:37:45 +0000

    files widget generator

--- Console/PropertiesUtils.php
+++ Console/PropertiesUtils.php
@@ -212,12 +212,18 @@ class PropertiesUtils
                     "template" => '<SecondsToTime seconds={TP_VALUE}/>'
                 ];
                 break;
-
             case 'status-short':
                 $compName = Utils::fixComponentName(ucfirst($property['schema']) . 'Statuses');
                 return [
                     "import" => 'import { ' . $compName . ' } from "../../statuses/badges/' . $compName . '";',
                     "template" => '{' . $compName . '(TP_VALUE, "TP_KEY", true)}'
+                ];
+                break;
+            case 'status':
+                $compName = Utils::fixComponentName(ucfirst($property['schema']) . 'Statuses');
+                return [
+                    "import" => 'import { ' . $compName . ' } from "../../statuses/badges/' . $compName . '";',
+                    "template" => '{' . $compName . '(TP_VALUE, "TP_KEY")}'
                 ];
                 break;
             case 'file':
