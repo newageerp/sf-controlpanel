7a98353f77023b3dc9a5306864ce75b2ec7519b3
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-07-21 05:23:58 +0000

    files widget generator

--- Controller/ConfigPropertiesController.php
+++ Controller/ConfigPropertiesController.php
@@ -13,6 +13,125 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class ConfigPropertiesController extends ConfigBaseController
 {
+    protected function getLocalStorageFile()
+    {
+        $file = $this->getLocalStorage() . '/propeties.json';
+        if (!file_exists($file)) {
+            file_put_contents($file, json_encode([]));
+        }
+        return $file;
+    }
+
+    protected function saveBuilder($data) {
+        file_put_contents(
+            $this->getLocalStorageFile(),
+            json_encode($data)
+        );
+    }
+
+    /**
+     * @Route(path="/listConfig", methods={"POST"})
+     */
+    public function listConfig(Request $request)
+    {
+        $request = $this->transformJsonBody($request);
+
+        $output = ['data' => []];
+
+        try {
+            $data = json_decode(
+                file_get_contents($this->getLocalStorageFile()),
+                true
+            );
+
+            if ($request->get('id')) {
+                $data = array_filter(
+                    $data,
+                    function ($item) use ($request) {
+                        return $item['id'] === $request->get('id');
+                    }
+                );
+            }
+
+            $output['data'] = array_values($data);
+        } catch (\Exception $e) {
+            $output['e'] = $e->getMessage();
+        }
+        return $this->json($output);
+    }
+
+    /**
+     * @Route(path="/saveConfig", methods={"POST"})
+     */
+    public function saveConfig(Request $request)
+    {
+        $request = $this->transformJsonBody($request);
+
+        $output = [];
+
+        try {
+            $item = $request->get('item');
+            if (!isset($item['id']) || !$item['id']) {
+                $item['id'] = Uuid::uuid4()->toString();
+            }
+
+            $isFound = false;
+            $data = json_decode(
+                file_get_contents($this->getLocalStorageFile()),
+                true
+            );
+            foreach ($data as &$el) {
+                if ($el['id'] === $item['id']) {
+                    $el = $item;
+                    $isFound = true;
+                }
+            }
+            if (!$isFound) {
+                $data[] = $item;
+            }
+            unset($el);
+
+            $this->saveBuilder($data);
+
+            $output['data'] = $item;
+        } catch (\Exception $e) {
+            $output['e'] = $e->getMessage();
+        }
+        return $this->json($output);
+    }
+
+    /**
+     * @Route(path="/removeConfig", methods={"POST"})
+     */
+    public function removeConfig(Request $request)
+    {
+        $request = $this->transformJsonBody($request);
+
+        $output = [];
+
+        try {
+            $id = $request->get('id');
+
+            $tmpData = json_decode(
+                file_get_contents($this->getLocalStorageFile()),
+                true
+            );
+            $data = [];
+            foreach ($tmpData as $el) {
+                if ($id !== $el['id']) {
+                    $data[] = $el;
+                }
+            }
+
+            $this->saveBuilder($data);
+
+            $output['data'] = [];
+        } catch (\Exception $e) {
+            $output['e'] = $e->getMessage();
+        }
+        return $this->json($output);
+    }
+
     /**
      * @Route(path="/for-filter", methods={"POST"})
      */
