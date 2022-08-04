5b0187c88df5876b52c1b4984095d1e10ec94a12
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-08-04 08:37:42 +0000

    status items

--- Console/In/InGeneratorStatuses.php
+++ Console/In/InGeneratorStatuses.php
@@ -85,7 +85,7 @@ class InGeneratorStatuses extends Command
                     'bgColor' => isset($status['config']['badgeVariant']) ? $status['config']['badgeVariant'] : ''
                 ];
                 $statusData[] = $el;
-                $statusJson[$entity][$type][] = $el;
+                $statusJson[$slug][$status['config']['type']][] = $el;
             }
 
             $fileName = $generatedPath . '/' . $compName . '.tsx';
