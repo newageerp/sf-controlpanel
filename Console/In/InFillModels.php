b74579b66f5347beb31ac85ff99d3e3529249487
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-09-19 06:57:33 +0000

    updates

--- Console/In/InFillModels.php
+++ Console/In/InFillModels.php
@@ -155,7 +155,7 @@ class InFillModels extends Command
         Utils::writeOnChanges($ormPath, $ormContents);
 
         // FILL ormSelectors
-        $selectorsPath = $modelsDir . "/ormSelectors.js";
+        $selectorsPath = $modelsDir . "/ormSelectors.ts";
 
         $ormSelectorsJsTemplate->writeToFileOnChanges(
             $selectorsPath,
