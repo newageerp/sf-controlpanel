00f153f284137984832b21e0df5df4669de7ef95
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-09-27 03:30:02 +0000

    updates

--- Service/Properties/PropertyDateService.php
+++ Service/Properties/PropertyDateService.php
@@ -21,49 +21,49 @@ class PropertyDateService
 
     public function generate()
     {
-        $tService = new TemplateService('v2/properties/dates/dates.html.twig');
+        // $tService = new TemplateService('v2/properties/dates/dates.html.twig');
 
-        $dates = [];
-        foreach ($this->properties as $property) {
-            if ($property['config']['type'] === 'string' && $property['config']['typeFormat'] === 'date') {
-                $entity = $property['config']['entity'];
-                if (!isset($dates[$entity])) {
-                    $dates[$entity] = [];
-                }
-                $dates[$entity][] = $property['config'];
-            }
-        }
+        // $dates = [];
+        // foreach ($this->properties as $property) {
+        //     if ($property['config']['type'] === 'string' && $property['config']['typeFormat'] === 'date') {
+        //         $entity = $property['config']['entity'];
+        //         if (!isset($dates[$entity])) {
+        //             $dates[$entity] = [];
+        //         }
+        //         $dates[$entity][] = $property['config'];
+        //     }
+        // }
 
-        foreach ($dates as $slug => $properties) {
-            $slugUc = Utils::fixComponentName($slug);
-            $slugPath = Utils::generatedV2Path('properties/' . $slugUc);
-            $componentName = Utils::fixComponentName($slug . 'Dates');
+        // foreach ($dates as $slug => $properties) {
+        //     $slugUc = Utils::fixComponentName($slug);
+        //     $slugPath = Utils::generatedV2Path('properties/' . $slugUc);
+        //     $componentName = Utils::fixComponentName($slug . 'Dates');
 
-            $parseProps = [];
-            foreach ($properties as $property) {
-                if ($this->defaultsService->isFieldExistsInDefaults($slug, $slug . '.' . $property['key'])) {
-                    $parseProps[] = $property;
-                }
-            }
+        //     $parseProps = [];
+        //     foreach ($properties as $property) {
+        //         if ($this->defaultsService->isFieldExistsInDefaults($slug, $slug . '.' . $property['key'])) {
+        //             $parseProps[] = $property;
+        //         }
+        //     }
 
-            if (count($parseProps) > 0) {
-                $parseProps = array_map(
-                    function ($item) use ($slug) {
-                        $item['compName'] = Utils::fixComponentName($slug . 'Date-' . $item['key']);
-                        return $item;
-                    },
-                    $parseProps
-                );
+        //     if (count($parseProps) > 0) {
+        //         $parseProps = array_map(
+        //             function ($item) use ($slug) {
+        //                 $item['compName'] = Utils::fixComponentName($slug . 'Date-' . $item['key']);
+        //                 return $item;
+        //             },
+        //             $parseProps
+        //         );
 
-                $tService->writeToFileOnChanges(
-                    $slugPath . '/' . $componentName . '.tsx',
-                    [
-                        'compName' => $componentName,
-                        'properties' => $parseProps,
-                        'slugUc' => $slugUc,
-                    ]
-                );
-            }
-        }
+        //         $tService->writeToFileOnChanges(
+        //             $slugPath . '/' . $componentName . '.tsx',
+        //             [
+        //                 'compName' => $componentName,
+        //                 'properties' => $parseProps,
+        //                 'slugUc' => $slugUc,
+        //             ]
+        //         );
+        //     }
+        // }
     }
 }
--- Service/Properties/PropertyTotalService.php
+++ Service/Properties/PropertyTotalService.php
@@ -17,42 +17,42 @@ class PropertyTotalService
 
     public function generate()
     {
-        $tService = new TemplateService('v2/properties/totals/totals.html.twig');
+        // $tService = new TemplateService('v2/properties/totals/totals.html.twig');
 
-        $totals = array_filter($this->properties, function ($item) {
-            return $item['config']['available_total'] === 1 || $item['config']['available_total'] === true;
-        });
+        // $totals = array_filter($this->properties, function ($item) {
+        //     return $item['config']['available_total'] === 1 || $item['config']['available_total'] === true;
+        // });
 
-        $totalsByEntity = [];
-        foreach ($totals as $total) {
-            $entity = $total['config']['entity'];
-            if (!isset($totalsByEntity[$entity])) {
-                $totalsByEntity[$entity] = [];
-            }
-            $totalsByEntity[$entity][] = $total;
-        }
+        // $totalsByEntity = [];
+        // foreach ($totals as $total) {
+        //     $entity = $total['config']['entity'];
+        //     if (!isset($totalsByEntity[$entity])) {
+        //         $totalsByEntity[$entity] = [];
+        //     }
+        //     $totalsByEntity[$entity][] = $total;
+        // }
 
-        foreach ($totalsByEntity as $slug => $totals) {
-            $slugUc = Utils::fixComponentName($slug);
-            $slugPath = Utils::generatedV2Path('properties/' . $slugUc);
-            $componentName = Utils::fixComponentName($slug . 'Totals');
+        // foreach ($totalsByEntity as $slug => $totals) {
+        //     $slugUc = Utils::fixComponentName($slug);
+        //     $slugPath = Utils::generatedV2Path('properties/' . $slugUc);
+        //     $componentName = Utils::fixComponentName($slug . 'Totals');
 
-            $totals = array_map(
-                function ($item) use ($slug) {
-                    $item['config']['compName'] = Utils::fixComponentName($slug . 'Totals-' . $item['config']['key']);
-                    return $item['config'];
-                },
-                $totals
-            );
+        //     $totals = array_map(
+        //         function ($item) use ($slug) {
+        //             $item['config']['compName'] = Utils::fixComponentName($slug . 'Totals-' . $item['config']['key']);
+        //             return $item['config'];
+        //         },
+        //         $totals
+        //     );
 
-            $tService->writeToFileOnChanges(
-                $slugPath . '/' . $componentName . '.tsx',
-                [
-                    'compName' => $componentName,
-                    'totals' => $totals,
-                    'slugUc' => $slugUc,
-                ]
-            );
-        }
+        //     $tService->writeToFileOnChanges(
+        //         $slugPath . '/' . $componentName . '.tsx',
+        //         [
+        //             'compName' => $componentName,
+        //             'totals' => $totals,
+        //             'slugUc' => $slugUc,
+        //         ]
+        //     );
+        // }
     }
 }
